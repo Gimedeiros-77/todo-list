a110e7179a0c92e624ac321749233fb34aefd6e2
Gimedeiros77 <giovanimedeiros2@gmail.com>
2024-12-30 11:18:38 -0300

    finalizando o projeto

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+import Todo from './components/Todo';
+
+import './App.css'
+import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
+import { use } from 'react';
+
 
 function App() {
   const [todos, setTodos] = useState([
@@ -21,29 +29,60 @@ function App() {
       category: "Estudos",
       isCompleted: false,
     }
-  ])
+  ]);
+
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState ("All");
+  const [sort, setSort] = useState ("Asc");
+
+  const addTodo = (text, category) => {
+    
+    const newTodos = [...todos,{
+      id: Math.floor(Math.random() *1000),
+      text,
+      category,
+      isCompleted:false,
+    },
+  ];
+
+  setTodos(newTodos);
+};
   
+  const removeTodo = (id)=> {
+    const newTodos = [...todos]
+    const fiteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null
+
+    );
+    setTodos (fiteredTodos);
+  };
+
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) =>todo.id ===id ? todo.isCompleted = !todo.isCompleted: todo);
+    setTodos(newTodos);
+
+    
+  }
 
   return (
     <div className="app">
-      <h1>lista De Tarefas</h1>
+      <h1>Lista de tarefas</h1>
+      <Search search ={search} setSearch={setSearch} />
+      <Filter filter ={filter} setFilter ={setFilter} setSort={setSort} />
       <div className="todo-list">
-        {todos.map((todo)=>(
-          <div className="todos">
-            <div className="content">
-              <p>{todo.text}</p>
-              <p className="category">
-                ({todo.category})
-              </p>
-            </div>
-            <div>
-              <button>Completar</button>
-              <button>X</button>
-            </div>
-          </div>
+        {todos
+        .filter((todos) => filter ==="All" ? true : filter === "Completed" ? todos.isCompleted :!todos.isCompleted)
+        .filter((todos) => todos.text.toLowerCase().includes(search.toLocaleLowerCase()
+        )
+      )
+        .sort((a, b)=> sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
+        .map((todo)=>(
+          <Todo key={todo.id} todo={todo} removeTodo ={removeTodo} completeTodo={completeTodo} />
         ))}
       </div>
-
+        <TodoForm addTodo={addTodo} />
     </div>
   )
 }
